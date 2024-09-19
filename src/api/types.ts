77ef24844fcc8ce0a3a0dd74de3a1425d81db156
undg <und3rdg@gmail.com>
2024-09-19 02:22:26 +0100

    implement all sinks

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,14 +1,28 @@
 // @TODO (undg) 2024-09-19: generate those types on the BE or generate them from GetSchema API provided by the server.
 
-export type WsMessage = {
-  action: 'GetSinks' | 'SetSinks' // and more
-  status: number // 400x
-  value?: GetSinks[] // and more
-  error?: string
-}
+type Action = 'GetSinks' | 'SetSinks' // and more
+
 export type GetSinks = {
+  /** uniq name, can be used as ID */
   name: string
   label: string
-  volume: number
+  /** volume is a number string */
+  volume: string
   muted: boolean
+}
+export type GetWsMessage = {
+  action: Action
+  status: number // 400x
+  payload?: GetSinks[] // and more
+  error?: string
+}
+
+type SendSink = {
+  name: string
+  volume: string
+}
+
+export type SendWsMessate = {
+  action: Action
+  payload: SendSink
 }
